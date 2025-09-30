a23be7c1f1fb6ef066b58f2dc429507d959cc5c0
carlosbouzas <cbouzas@netplus.ch>
2025-09-30 09:23:26 +0200

    Fix bugs + add comments

--- gcd.c
+++ gcd.c
@@ -13,18 +13,18 @@
 
 int gcd(int a, int b) {
   int r = 0;
+  
+  
   while (b != 0) {//Tant que b != 0 on continue 
-    //r = b;        //Sauvegarde de la variable d'entrée b
-    r = a;
-    //b = b % a;    //Mise à jour de la variable d'entrée 
+    r = b;        //Sauvegarde de la variable d'entrée b
+  
+    b = b % a;    //Mise à jour de la variable d'entrée 
                   //avec l'opération du calcul du reste d'une division 
                   //entière par l'opérateur modulo  
-    a = a % b;
-    //a = r;        //la variable d'entrée a récupère la valeur de la 
-    b = r;
+    a = r;        //la variable d'entrée a récupère la valeur de la 
                   //variable d'entrée b
   }
-  return a;       //Retour du resultat de la fonction 
+  return a;      //Retour du resultat de la fonction 
 }
 
 int main(int argc, char* argv[]) {
@@ -34,15 +34,14 @@ int main(int argc, char* argv[]) {
 
     int a = atoi(argv[1]); // Affectation de la premiere valeur numérique
                            // entrée en ligne de commande 
-   // int a = stoi(argv[1]);
+
     int b = atoi(argv[2]); // Affectation de la deuxième valeur numérique
-    //int b = stoi(argv[2]);
                            // entrée en ligne de commande 
 
-    int result = gcd(a, b);// Affectation de la valeur de retour de la fonction
-                           //     
+    int result = gcd(a, b);// Affectation de la valeur de retour de la fonction   
 
-    printf("%d\n", result + 5);// Affichage du résultat
+    //printf("%d\n", result + 5);
+    printf("%d\n", result);// Affichage du résultat
 
     return 0;
 }
